ec5e958b8a2ec5aab7d3c2856f0d72ff299fd2d4
Yannick Francois <yaf@elsif.fr>
2020-02-17 17:11:44 +0100

    Ajoute une page d'information pour les 3 groupes
    Co-authored-by: pblayo <philippe.blayo@beta.gouv.fr>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,3 @@
 class PagesController < ApplicationController
 
-  def accueil
-
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :parcours
   resources :domaines, only: :index
   root to: "pages#accueil"
+
   get "entreprise", to: "pages#entreprise"
+  get "acheteur", to: "pages#acheteur"
+  get "tuteur", to: "pages#tuteur"
 end
